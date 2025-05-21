dd18c8358e7fe50f074b9b12841b57751b4b91ef
Abhith B N <ABHITH.BN@cognizant.com>
2025-05-21 15:24:37 +0530

    first commit added most files

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CredWise_Trail.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Landing()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CredWise_Trail.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult CustomerDashboard()
+        {
+            return View();
+        }
+        public IActionResult LoanApplication()
+        {
+            return View();
+        }
+        public IActionResult CustomerStatements()
+        {
+            return View();
+        }
+        public IActionResult LoanStatus()
+        {
+            return View();
+        }
+        public IActionResult KYCUpload()
+        {
+            return View();
+        }
+        public IActionResult CustomerDetails()
+        {
+            return View();
+        }
+        public IActionResult CustomerUpdate()
+        {
+            return View();
+        }
+        public IActionResult MakePayment()
+        {
+            return View();
+        }
+    }
+}
