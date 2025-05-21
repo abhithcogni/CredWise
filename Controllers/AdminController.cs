03f3adf0b532b1e6a0389a2bb10c5ed75cd0b863
Abhith B N <ABHITH.BN@cognizant.com>
2025-05-21 17:19:40 +0530

    admin portal added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,7 +4,23 @@ namespace CredWise_Trail.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public IActionResult KYCApproval()
+        {
+            return View();
+        }
+        public IActionResult LoanApproval()
+        {
+            return View();
+        }
+        public IActionResult AddLoanProduct()
+        {
+            return View();
+        }
+        public IActionResult LoanProducts()
         {
             return View();
         }
